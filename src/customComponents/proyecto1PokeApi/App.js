7473e345babda6fda823a36af83cd7db6d778e93
Adrianpogo <adrianpogo15@gmail.com>
2025-01-15 14:12:07 +0100

    inicio Proyecto Pokeapi

---
+++ src/customComponents/proyecto1PokeApi/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {useState } from "react";
+import { Formulario } from "../Formulario";
+import tarjeta from "../tarjeta.png";
+
+export const App = () => {
+    return  <>
+                <div></div>
+            </>
+        
+    
+}
---
+++ src/customComponents/proyecto1PokeApi/Tarjeta.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Tarjeta = ({pokemon}) => {
+    
+}
