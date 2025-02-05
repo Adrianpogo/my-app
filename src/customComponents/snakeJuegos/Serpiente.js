3e0a4712647c694bc4f8ba8dc600c884ee2033fd
Adrianpogo <adrianpogo15@gmail.com>
2025-02-05 14:10:59 +0100

    modificaciones Snake

--- src/customComponents/snakeJuegos/Serpiente.js
+++ src/customComponents/snakeJuegos/Serpiente.js
@@ -6,7 +6,7 @@ import colaImg from "./img/tail.png";
 export const Serpiente = ({ serpiente }) => {
 
     return serpiente.map((parteSerpiente, index) => {
-        var imgSrc = index == 0 ? cabezaImg : index = serpiente.length - 1 ? colaImg : cuerpoImg
+        var imgSrc = index === 0 ? cabezaImg : index === serpiente.length - 1 ? colaImg : cuerpoImg;
         return <>
             <div key={index} className="celda" style={{ gridColumn: parteSerpiente.x + 1, gridRow: parteSerpiente.y + 1 }}>
                 <img src={imgSrc} alt="Serpiente" className="img-fluid"></img>
